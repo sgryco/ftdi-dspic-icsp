55fa7812f4076e4ed5f4fd84070887b135615d78
Corentin Chéron <cc@airinov.fr>
2016-08-29 00:33:44 +0200

    WIP Led is still not working

--- icsp_for_dspic33e/led_blink_icsp/led.c
+++ icsp_for_dspic33e/led_blink_icsp/led.c
@@ -37,21 +37,17 @@ void test_key(){
 void test_led(){
   open_ftdi_for_icsp();
   enter_icsp(); //key + 5 clocks + six(goto_0x200) + flush
-	six(nop);
-  six(goto_0x200);
-  six(nop);
-  six(nop);
-  six(nop);
+  read_id();
+  test_regout();
   printf("BCLR TRISD #0\n");
   six(0xA90E30); // A90E30     BCLR TRISD, #0
+  six(nop);
   printf("A80E34     BSET LATD, #0\n");
   six(0xA80E34);
   six(nop);
   flush_buf();
   printf("LED ON ?! (3s)\n");
-  sleep(1);
-  read_id();
-  test_regout();
+  sleep(3);
   exit_icsp();
   close_ftdi_for_icsp();
 }
